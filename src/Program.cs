6571745c2f528fe3b14be162ce52e6ce38bd21f7
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-03-13 10:01:15 +0100

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -8,7 +8,7 @@ class Program
         Console.WriteLine("Hello, World!");
 
         // Simulate usage of a hardcoded password (trigger for Snyk)
-        string password = "mySuperSecretPassword12vg5545eedssg";
+        string password = "mySuperSecretPassword12vg5dgfe545eedssg";
         Console.WriteLine($"Password: {password}");
     }
 }
